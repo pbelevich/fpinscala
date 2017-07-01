11b4e312e0cf1b5b0a0ee6cac8f5aa8dba1c0c05
Pavel Belevich <belevichp@gmail.com>
2017-07-01 17:24:08 -0400

    Option.map,flatMap,getOrElse,orElse,filter

--- src/main/scala/fpinscala/fpinscala/errorhandling/Option.scala
+++ src/main/scala/fpinscala/fpinscala/errorhandling/Option.scala
@@ -4,18 +4,36 @@ package fpinscala.fpinscala.errorhandling
   * @author Pavel Belevich
   */
 
-import scala.{Option => _, Some => _, Either => _, _}
+import scala.{Either => _, Option => _, Some => _, _}
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = ???
 
-  def flatMap[B](f: A => Option[B]) = ???
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
 
-  def getOrElse[B >: A](default: => B): B = ???
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(a) => f(a)
+  }
 
-  def orElse[B >: A](ob: => Option[B]): Option[B] = ???
+  def flatMap_1[B](f: A => Option[B]): Option[B] = map(f) getOrElse None
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
+    case _ => this
+  }
+
+  def orElse_1[B >: A](ob: => Option[B]): Option[B] = map(Some(_)) getOrElse ob
+
+  def filter(f: A => Boolean): Option[A] = flatMap(a => if (f(a)) this else None)
+
 }
 
 case class Some[+A](get: A) extends Option[A]
---
+++ src/test/scala/fpinscala/fpinscala/errorhandling/OptionTest.scala
@@ -0,0 +1,52 @@
+package fpinscala.fpinscala.errorhandling
+
+import org.scalatest.FunSuite
+
+/**
+  * @author Pavel Belevich
+  */
+class OptionTest extends FunSuite {
+
+  test("testOrElse") {
+    assert(Some(42) == (None: Option[Int]).orElse(Some(42)))
+    assert(None == (None: Option[Int]).orElse(None))
+    assert(Some(1) == (Some(1): Option[Int]).orElse(Some(42)))
+    assert(Some(1) == (Some(1): Option[Int]).orElse(None))
+  }
+
+  test("testOrElse_1") {
+    assert(Some(42) == (None: Option[Int]).orElse_1(Some(42)))
+    assert(None == (None: Option[Int]).orElse_1(None))
+    assert(Some(1) == (Some(1): Option[Int]).orElse_1(Some(42)))
+    assert(Some(1) == (Some(1): Option[Int]).orElse_1(None))
+  }
+
+  test("testFilter") {
+    assert(None == (None: Option[Int]).filter(_ % 2 == 0))
+    assert(None == (Some(1): Option[Int]).filter(_ % 2 == 0))
+    assert(Some(2) == (Some(2): Option[Int]).filter(_ % 2 == 0))
+  }
+
+  test("testFlatMap") {
+    assert(None == (None: Option[Int]).flatMap(a => Some(a + 1)))
+    assert(Some(2) == (Some(1): Option[Int]).flatMap(a => Some(a + 1)))
+    assert(None == (Some(1): Option[Int]).flatMap(_ => None))
+  }
+
+  test("testFlatMap_1") {
+    assert(None == (None: Option[Int]).flatMap_1(a => Some(a + 1)))
+    assert(Some(2) == (Some(1): Option[Int]).flatMap_1(a => Some(a + 1)))
+    assert(None == (Some(1): Option[Int]).flatMap_1(_ => None))
+  }
+
+  test("testGetOrElse") {
+    assert(42 == (None: Option[Int]).getOrElse(42))
+    assert(1 == (Some(1): Option[Int]).getOrElse(42))
+  }
+
+  test("testMap") {
+    assert(None == (None: Option[Int]).map(_ + 1))
+    assert(Some(2) == (Some(1): Option[Int]).map(_ + 1))
+  }
+
+}
